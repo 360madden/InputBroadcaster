978000f9edab92f9282b50c19b55381f051a3f97
360madden <360madden@gmail.com>
2026-03-31 13:40:10 -0400

    feat(core): add broadcast key definitions

---
+++ src/InputBroadcaster.Core/BroadcastKey.cs
@@ -0,0 +1,18 @@
+namespace InputBroadcaster.Core;
+
+public enum BroadcastKey
+{
+    D1,
+    D2,
+    D3,
+    D4,
+    D5,
+    D6,
+    D7,
+    D8,
+    D9,
+    D0,
+    OemMinus,
+    OemPlus,
+    Unknown,
+}
